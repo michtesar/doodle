cfd7ba593edc5469468530a9767cd4d501e56e06
Michael Tesar <michtesar@gmail.com>
2023-03-16 16:29:26 +0100

    Fix predictions

--- sketch.js
+++ sketch.js
@@ -1,45 +1,61 @@
-let backgroundColor
-let doodleClassifier
-let canvas
+let doodleClassifier;
+let canvas;
 
 function setStatus(text) {
-  document.getElementById("status").innerHTML = text
+  document.getElementById("status").innerHTML = text;
 }
 
 function setup() {
-  canvas = createCanvas(600, 600)
-  canvas.parent("canvas")
-  backgroundColor = color(255, 255, 255)
-  background(backgroundColor)
-  doodleClassifier = ml5.imageClassifier("DoodleNet", modelReady)
+  canvas = createCanvas(400, 400);
+  canvas.parent("canvas");
+  background(255);
+  doodleClassifier = ml5.imageClassifier("DoodleNet", modelReady);
 }
 
 function draw() {
   if (mouseIsPressed) {
-    stroke(0)
-    strokeWeight(16)
-    line(mouseX, mouseY, pmouseX, pmouseY)
+    stroke(0);
+    strokeWeight(16);
+    line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
 
 function clearCanvas() {
-  background(backgroundColor)
+  background(255);
 }
 
 function modelReady() {
-  doodleClassifier.classify(canvas, gotResults)
+  doodleClassifier.classify(canvas, gotResults);
+}
+
+function formatConfidence(number) {
+  return nf(100 * number, 1, 2);
+}
+
+function formatLabel(label) {
+  return label.replace("_", " ");
+}
+
+function formatResults(results, n = 2) {
+  let text = "";
+  for (let i = 0; i < n; i++) {
+    let result = results[i];
+    text += `
+    ${formatLabel(result.label)}
+     ${formatConfidence(result.confidence)}%`;
+    if (result !== n) {
+      text += "<br>";
+    }
+  }
+  return text;
 }
 
 function gotResults(error, results) {
   if (error) {
-    throw error
+    throw error;
   }
-  console.log(results[0])
-  let resultsText = `${results[0].label} ${nf(
-    100 * results[0].confidence,
-    2,
-    0
-  )}%`
-  setStatus(resultsText)
-  doodleClassifier.classify(canvas, gotResults)
+  console.log(results[0]);
+  predictions = formatResults(results, 2);
+  setStatus([predictions]);
+  doodleClassifier.classify(canvas, gotResults);
 }
